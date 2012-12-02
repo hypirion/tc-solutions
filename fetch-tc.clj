7d116f74ba37de787d44754319418e836adbd10b
Jean Niklas L'orange <jeannikl@hypirion.com>
2012-12-02 14:43:28 +0100

    Don't fetch solved problems.

--- fetch-tc.clj
+++ fetch-tc.clj
@@ -8,12 +8,15 @@
            (.listFiles)
            (filter #(.isDirectory %))
            (filter #(.. % getName (endsWith "-java"))))
+      already-copied (set (map #(.getName %)
+                               (.listFiles (File. "."))))
       fnames (set (map #(.toUpperCase
                          (str (.getPath %) \/
                               (.. % getName (replace \- \.)))) dirs))]
   (->> dirs
        (mapcat #(.listFiles %))
        (filter #(contains? fnames (.. % getPath toUpperCase)))
+       (remove #(contains? already-copied (.getName %)))
        (mapv (fn [f] (with-open [r (io/reader f)]
                       (with-open [w (io/writer (.getName f))]
                         (io/copy r w)))))))
